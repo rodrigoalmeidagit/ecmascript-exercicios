65522c4a69d73097f95a25163e1f10284e9c92b3
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-09-14 00:24:04 -0300

    complete exercicio6.js

--- capitulo-03/exercicio6.js
+++ capitulo-03/exercicio6.js
@@ -1,7 +1,13 @@
 var alunos = [
-  { nome: "Diogo", media: 5.5 },
-  { nome: "Julia", media: 9.5 },
   { nome: "Roberto", media: 1.5 },
+  { nome: "Fernando", media: 3.5 },
+  { nome: "Diogo", media: 5.5 },
   { nome: "Tiago", media: 6.0 },
+  { nome: "Renata", media: 7.8 },
+  { nome: "Cristian", media: 8.3 },
+  { nome: "Julia", media: 9.5 },
 ];
 
+function aprovados(alunos, nota) {
+  return alunos.filter(aluno => aluno.media >= nota)
+}
