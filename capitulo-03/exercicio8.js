6df379587e3e0fc611bbfbbb054f6037565ed06f
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-09-15 22:55:26 -0300

    complete exercicio8.js

--- capitulo-03/exercicio8.js
+++ capitulo-03/exercicio8.js
@@ -4,3 +4,10 @@ var dimensoes = [
   { altura: 1, comprimento: 1 },
   { altura: 50, comprimento: 50 },
 ];
+
+function calculaAreaTotal(array) {
+  return array.reduce((acc, { altura, comprimento }) =>
+    acc += altura * comprimento, 0)
+}
+
+console.log(calculaAreaTotal(dimensoes));
