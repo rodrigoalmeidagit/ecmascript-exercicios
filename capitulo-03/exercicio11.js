2dc54d745735b7d124eec4dc9d50fb28a26629d2
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-09-16 16:17:18 -0300

    complete exercicio11.js

--- capitulo-03/exercicio11.js
+++ capitulo-03/exercicio11.js
@@ -1,1 +1,17 @@
-const clonarObjeto = () => { }
+const clonarObjeto = (obj) => {
+  const prop = Object.getOwnPropertyNames(obj)
+  const copy = {}
+  prop.forEach(element => copy[element] = obj[element])
+
+  return copy
+}
+
+const objeto = {
+  nome: 'rodrigo',
+  idade: 20
+}
+
+const newObject = clonarObjeto(objeto)
+
+console.log('Objeto Original', objeto);
+console.log('Objeto Cópia', newObject);
